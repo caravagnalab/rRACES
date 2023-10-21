b10a895131c82c43ace3ede821a700b743e6233d
Giulio Caravagna <gcaravagn@gmail.com>
2023-10-21 17:11:17 +0200

    Shiny App draft

--- R/rRACES-package.R
+++ R/rRACES-package.R
@@ -4,6 +4,7 @@
 ## usethis namespace: start
 #' @importFrom dplyr rename
 #' @importFrom ggplot2 ggplot
+#' @importFrom jsonlite toJSON
 #' @importFrom readr read_tsv
 ## usethis namespace: end
 NULL
