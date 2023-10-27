f3ee65c550edd6e11f287c312c83f11aeb9d0cbf
Giulio Caravagna <gcaravagn@gmail.com>
2023-10-28 01:18:42 +0200

    Removed files from local tests

--- _builds/CMakeFiles/libRACES_static.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libRACES.a"
-  "libRACES.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/libRACES_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
