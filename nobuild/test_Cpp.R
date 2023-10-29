74c4b2407c7b4b3c582435b2b155a40bc1b10e1f
Alberto Casagrande <acasagrande@units.it>
2023-10-29 18:20:00 +0100

    Closes #18, closes #19; Improved `Simulation` API - updated rRACES version - renamed `Simulation::run_up_to` into `run_up_to_time` - improved exception handling in `Simulation::run_up_to_time` - added `Simulation::run_up_to_size(...)` as requested by issue #19 - updated tests and `rRACES.Rmd`

--- nobuild/test_Cpp.R
+++ nobuild/test_Cpp.R
@@ -1,9 +1,24 @@
 devtools::load_all()
 
 # Create a new simulation and set the simulation data directory
-# to be "output_dir". A 1000x1000-cells tissue is automatically
+# to be "test". A 1000x1000-cells tissue is automatically
 # added to the simulation
-sim <- new(Simulation, "output_dir")
+sim <- new(Simulation, "test")
+
+# Get the simulation directory, i.e., "test"
+sim$get_directory()
+
+# Get the tissue size, i.e., c(1000,1000)
+sim$get_tissue_size()
+
+# Set the tissue name and size
+sim$set_tissue("Liver", 1200, 900)
+
+# Get the tissue name, i.e., "Liver"
+sim$get_tissue_name()
+
+# Get the tissue size, i.e., c(1200,900)
+sim$get_tissue_size()
 
 # Add two species, "A" and "B", having epigenetic states
 sim$add_species(name = "A",
@@ -17,7 +32,7 @@ sim$add_species(name = "B",
                 death_rates = c("+" = 0.05, "-" = 0.05))
 
 # Program a timed transition from species "A" to
-# species "B" at time 60
+# species "B" at time 30
 sim$add_timed_mutation("A", "B", 60)
 
 # Get the simulation species with epigenetic state names
@@ -26,10 +41,25 @@ sim$get_species_names()
 # Add one cell to the simulated tissue
 sim$add_cell(genotype_name = "A+", x = 500, y = 500)
 
-# Run the simulation up to time 110
-sim$run_up_to(110)
+# Run the simulation up until the there are less than 100 
+# cells in the species B-
+sim$run_up_to_size("B-", 100)
 
-# Get simulation time (you will get 110)
+# Get simulation time
+sim$get_clock()
+
+# Report the number of cells per species at the current simulation time, e.g.,
+#   species epistate counts
+# 1       A        -  30942
+# 2       A        + 105715
+# 3       B        -    100
+# 4       B        +   1371
+sim$get_counts()
+
+# Run the simulation up to time 110
+sim$run_up_to_time(110)
+
+# Get simulation time, i.e., 110
 sim$get_clock()
 
 # Get the cells in the tissue at current simulation time
@@ -46,17 +76,5 @@ sim$get_cells(c("A", "B"), c("-"))
 # the same time, belonging to rectangular sample bounded by [500,500]
 # and [505,505] as lower and upper corners, respectively
 sim$get_cells(c(500, 500), c(505, 505), c("A", "B"), c("-"))
-
-# Report the number of cells per species at the current simulation time
-sim$get_counts()
-
-# Get the simulation directory
-sim$get_directory()
-
-# Get the tissue name
-sim$get_tissue_name()
-
-# Get the tissue size
-sim$get_tissue_size()
 
 # plot_simulation_counts(sim)
