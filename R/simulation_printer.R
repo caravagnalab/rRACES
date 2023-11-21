d66f1eb24522d7e6cab472c7f35787f35e658cba
Alberto Casagrande <acasagrande@units.it>
2023-11-21 17:35:58 +0100

    Added license to the source files

--- R/simulation_printer.R
+++ R/simulation_printer.R
@@ -1,3 +1,19 @@
+## This file is part of the rRACES (https://github.com/caravagnalab/rRACES/).
+## Copyright (C) 2023 - Alberto Casagrande <alberto.casagrande@uniud.it>
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #' @importFrom rlang .data
 setMethod("show", "Rcpp_Simulation", function(object) {
   sim_status <- (length(object$get_species())
@@ -50,7 +66,6 @@ setMethod("show", "Rcpp_Simulation", function(object) {
     my_tab <- species %>%
       dplyr::left_join(counts_tab, by = c("genotype", "epistate")) %>%
       dplyr::mutate(species = paste0(.data$genotype, .data$epistate)) %>%
-      #dplyr::select(.data$species, ancestor, time, .data$growth_rate,
       dplyr::select(.data$species, .data$growth_rate,
                     .data$death_rate, .data$switch_rate, .data$counts)
     colnames(my_tab)[2:4] <- c(" \u03BB ", " \u03B4 ", " \u03B5 ")
