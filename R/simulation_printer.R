dfa2e4b0fa7b31add194277db8cf473e6a34fa5e
Giulio Caravagna <gcaravagn@gmail.com>
2023-11-29 17:08:41 +0100

    Better printer method and other things
    - better show
    - vignette updates

--- R/simulation_printer.R
+++ R/simulation_printer.R
@@ -16,9 +16,13 @@
 
 #' @importFrom rlang .data
 setMethod("show", "Rcpp_Simulation", function(object) {
+  # If it can be simulated
   sim_status <- (length(object$get_species())
                  & nrow(object$get_cells()))
-  sam_status <- FALSE
+  
+  # If it has samples assigned
+  sam_status <- sim_status & nrow(object$get_samples_info())
+  
   mut_status <- FALSE
 
   sim_status <- ifelse(
@@ -61,17 +65,92 @@ setMethod("show", "Rcpp_Simulation", function(object) {
     cat('\n')
     cli::cli_alert_danger("The simulation has no species yet!")
   } else {
-    counts_tab <- object$get_counts()
+    counts_tab <- object$get_counts() %>% 
+      dplyr::mutate(
+        `%` = 100 * counts/sum(counts)
+      )
 
     my_tab <- species %>%
       dplyr::left_join(counts_tab, by = c("genotype", "epistate")) %>%
       dplyr::mutate(species = paste0(.data$genotype, .data$epistate)) %>%
       dplyr::select(.data$species, .data$growth_rate,
-                    .data$death_rate, .data$switch_rate, .data$counts)
+                    .data$death_rate, .data$switch_rate, .data$counts, .data$`%`)
     colnames(my_tab)[2:4] <- c(" \u03BB ", " \u03B4 ", " \u03B5 ")
+    
+    nspecies = nrow(my_tab)
+    with_epigenetics = max(nchar(species$epistate)) != 0
+    
+    cli::cli_h3(text = "Species: {.field {nspecies}}, {.field {ifelse(with_epigenetics, crayon::green('with'), crayon::red('without'))}} epigenetics")
 
     cat("   ",
         knitr::kable(my_tab, format = "rst", align = "rcrccc"),
         sep = "\n   ")
+    
+    # cli::cli_rule(center = "Firings")
+    
+    f_table = object$get_firings() %>%
+      dplyr::mutate(species = paste0(.data$genotype, .data$epistate))
+    
+    cli::cli_h3(text = paste("Firings:", sum(f_table$fired), 'total'))
+    
+    nch = f_table$fired %>% nchar %>% max
+    
+    for(s in my_tab$species)
+    {
+      s_ftab = f_table %>% dplyr::filter(species == s) %>% dplyr::arrange(event)
+      
+      if(with_epigenetics)
+        sprintf(
+          paste0('\n\tSpecies [%s]: %', nch, 's (death), %', nch, 's (growth) and %', nch, 's (switches)'),
+          s,
+          s_ftab$fired[1],
+          s_ftab$fired[2],
+          s_ftab$fired[3]
+        ) %>% cat()
+      else
+          sprintf(
+            paste0('\n\tSpecies [%s]: %', nch, 's (death) and %', nch, 's (growth)'),
+            s,
+            s_ftab$fired[1],
+            s_ftab$fired[2]
+          ) %>% cat()
+      
+    }
+    
+    # cat("   ",
+    #     knitr::kable(object$get_firings(), format = "rst", align = "rcrccc"),
+    #     sep = "\n   ")
+    
+  }
+  
+  
+  samples <- object$get_samples_info()
+  if (nrow(samples) == 0) {
+    cat('\n')
+    cli::cli_alert_danger("The simulation has no samples yet!")
+  } else {
+    t_samples = samples$time %>% unique()
+    
+    is_longitudinal = length(t_samples) > 1
+    is_multiregion = any(table(samples$time) > 1)
+    
+    is_longitudinal = ifelse(is_longitudinal, crayon::bgGreen(crayon::white(" Yes ")), crayon::bgRed(crayon::black(" No ")))
+    is_multiregion = ifelse(is_multiregion, crayon::bgGreen(crayon::white(" Yes ")), crayon::bgRed(crayon::black(" No ")))
+
+    cat("\n")
+    
+    ncells = samples$tumoural.cells %>% sum
+    
+    cli::cli_h3(text = "Samples ({.emph multi-region} {is_multiregion}, {.emph longitudinal} {is_longitudinal}): {.field {ncells}} cells from {.field {nrow(samples)}} samples at {.field {length(t_samples)}} timepoints")
+    # 
+    # 
+    # cli::cli_alert_info(
+    #   "There are {.field {nrow(samples)}} samples from {.field {length(t_samples)}} timepoints."
+    # )
+    
+    cat("   ",
+        knitr::kable(samples, format = "rst", align = "rcrccc"),
+        sep = "\n   ")
+ 
   }
 })
