a25424d96ad73004e9461acbfc64f821a4533c98
Alberto Casagrande <acasagrande@units.it>
2024-03-15 17:38:20 +0100

    Closed #92. Fixed `plot_exposure_timeline` - added `plot_exposure_timeline` to `_pkgdown.yml` - added `plot_exposure_timeline` to `plot_exposure_timeline` - `plot_exposure_timeline` now takes a phylogenetic forest as a parameter - added the optional parameter `linewidth` - added the optional Boolean parameter `emphatize_switches` - added an example

--- R/plot_exposure_timeline.R
+++ R/plot_exposure_timeline.R
@@ -14,48 +14,118 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-preprocess <- function(x) {
-  time_points <- unique(x$time)
-  a <- apply(x, 1, function(x) time_points[which(time_points == as.numeric(x[1])) + 1] )
-  a[is.na(a)] <- max(time_points) #+ mean(diff(time_points))
-  x$endtime <- a
-  x <- x[, c("time", "signature", "exposure", "type", "endtime")]
-  return(x)
+preprocess <- function(phylo_forest) {
+  exposure <- phylo_forest$get_exposures()
+
+  time_points <- exposure%>% dplyr::pull(time) %>% unique
+  sbses <- exposure %>% dplyr::pull(signature) %>% unique
+  for (t in time_points) {
+    for (sbs in sbses) {
+      if (nrow(exposure %>%
+                 filter(.data$time == t,
+                        .data$signature == sbs)) == 0) {
+        exposure[nrow(exposure)+1,] <- c(as.numeric(t), sbs,
+                                         as.numeric(0), "SBS")
+      }
+    }
+  }
+
+  last_time <- max(phylo_forest$get_samples_info()["time"])
+  time_points <- sort(unique(append(time_points, last_time)))
+  end_time <- apply(exposure, 1, function(x) {
+    time_points[which(time_points == as.numeric(x[1])) + 1]
+  })
+  end_time[is.na(end_time)] <- as.numeric(last_time)
+
+  exposure$end_time <- end_time
+
+  exposure <- exposure[order(exposure$time, exposure$signature), ]
+
+  exposure[, c(1, 3)] <- sapply(exposure[, c(1, 3)], as.numeric)
+
+  return(exposure)
 }
 
 #' Plot a signatures exposures
 #'
 #' @description
-#' Plots a signatures exposures in different time stamps
+#' Plots the signatures exposure changes along a phylogenetic
+#' forest.
 #'
-#' @param x exposure dataframe
+#' @param phylogenetic_forest A phylogenetic forest.
+#' @param linewidth The width of the lines in the plot.
 #'
 #' @return An editable ggplot plot.
+#' @examples
+#' sim <- new(Simulation)
+#'
+#' sim$add_mutant(name = "A",
+#'                growth_rates = 0.2,
+#'                death_rates = 0.0)
+#' sim$place_cell("A", 500, 500)
+#'
+#' sim$run_up_to_time(60)
+#'
+#' # sampling tissue
+#'
+#' n_w <- n_h <- 50
+#' ncells <- 0.8 * n_w * n_h
+#' bbox <- sim$search_sample(c("A" = ncells), n_w, n_h)
+#'
+#' sim$sample_cells("Sampling", bbox$lower_corner, bbox$upper_corner)
+#'
+#' forest <- sim$get_samples_forest()
+#'
+#' # placing mutations
+#'
+#' m_engine <- build_mutation_engine(setup_code = "demo")
+#'
+#' m_engine$add_mutant(mutant_name = "A",
+#'                     passenger_rates = c(SNV = 8e-8),
+#'                     driver_SNVs = c(), driver_CNAs = c())
+#'
+#' m_engine$add_exposure(c(SBS1 = 0.2, SBS5 = 0.8))
+#' m_engine$add_exposure(time = 50,
+#'                       c(SBS5 = 0.3, SBS2 = 0.2, SBS3 = 0.5))
+#' phylo_forest <- m_engine$place_mutations(forest, 500)
+#'
+#' plot_exposure_timeline(phylo_forest)
 #' @export
-plot_exposure_timeline <- function(x) {
-  
-  signames <- x %>%dplyr::pull(signature) %>% unique
+plot_exposure_timeline <- function(phylogenetic_forest, linewidth = 0.8,
+                                   emphatize_switches = FALSE) {
+  stopifnot(inherits(phylogenetic_forest, "Rcpp_PhylogeneticForest"))
+
+  exposure_df <- preprocess(phylogenetic_forest)
+
+  signames <- exposure_df %>% dplyr::pull(signature) %>% unique
   all_colors <- rRACES:::get_signatures_colors()
   colors <- all_colors[signames]
-  
-  x_prep <- preprocess(x)
-  
-  p <- ggplot2::ggplot(data = x_prep, ggplot2::aes(x = time, y = exposure, group = signature, color = signature)) + 
-    ggplot2::geom_segment(ggplot2::aes(xend = endtime, yend = exposure), linewidth = 0.5, position = ggplot2::position_jitter(width = 0.04, height = 0.04, seed = 10)) + 
-    ggplot2::geom_point(position = ggplot2::position_jitter(width = 0.04, height = 0.04, seed = 10)) + 
-    ggplot2::ylim(0, 1) + 
-    ggplot2::labs(x = "Time Point", y = "Signature Exposure", title = "Mutational Signature Exposure Timeline") + 
-    #scale_fill_manual(values=colors) + 
-    ggplot2::scale_colour_manual(name = "sigs",values = colors) + 
+
+  line_shift <- 0.005 * linewidth
+
+  exposure_df <- exposure_df %>% dplyr::group_by(time,exposure) %>%
+    dplyr::mutate(line_pos = .data$exposure + line_shift * (row_number() - 1))
+
+  p <- ggplot2::ggplot(data = exposure_df,
+                       ggplot2::aes(x = time, y = .data$line_pos,
+                                    group = signature, color = signature)) +
+    ggplot2::geom_segment(ggplot2::aes(xend = .data$end_time,
+                                       yend = .data$line_pos),
+                          linewidth = linewidth) +
+    ggplot2::ylim(0, 1) +
+    ggplot2::labs(x = "Time Point", y = "Signature Exposure",
+                  title = "Mutational Signature Exposure Timeline") +
+    #scale_fill_manual(values=colors) +
+    ggplot2::scale_colour_manual(name = "Signatures", values = colors) +
     ggplot2::theme_minimal()  # Apply a minimal theme
-  
-  #ggplot(data = x, aes(x = time, y = exposure, group = signature, color = signature)) +
-  #  geom_step(direction = "hv", linewidth = 1, position = position_jitter(width = 0.05, height = 0.05, seed = 10)) +  # Connect points with horizontal lines
-  #  #geom_point(size = 3, position = position_jitter(width = 0.00, height = 0.004, seed = 10)) +  # Add points
-  #  ylim(0, 1) + 
-  #  labs(x = "Time Point", y = "Signature Exposure", title = "Mutational Signature Exposure Timeline") +
-  #  theme_minimal()  # Apply a minimal theme
-  
+
+  if (emphatize_switches) {
+    switching_times <- exposure_df["time"] %>% filter(time != 0)
+    for (switching_time in switching_times) {
+      p <- p +
+        ggplot2::geom_vline(xintercept = switching_time, linetype = "dotted")
+    }
+  }
+
   return(p)
 }
-
