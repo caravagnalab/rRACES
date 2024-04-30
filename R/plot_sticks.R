34ae06d7405e8c5caa53de7732cfb54642e8daa5
riccardobergamin <ric.b.91@gmail.com>
2024-04-30 18:38:45 +0200

    exported functions in name space

--- R/plot_sticks.R
+++ R/plot_sticks.R
@@ -12,13 +12,23 @@
 #' @export
 #'
 #' @examples
-#' sim <- new(Simulation)
-#' sim$add_mutant(name = "A", growth_rates = 0.08, death_rates = 0.01)
-#' sim$place_cell("A", 500, 500)
-#' sim$run_up_to_time(60)
-#' sim$sample_cells("MySample", c(500, 500), c(510, 510))
+#'sim <- new(Simulation, "homogeneous_test",seed = sample(x = 1:5000,size = 1,replace = F),
+#'            save_snapshot = F)
+#' sim$duplicate_internal_cells <- T
+#' sim$update_tissue("Liver", 2e3, 2e3)
+#' sim$add_mutant(name = "A",
+#'                growth_rates = 1,
+#'                death_rates = 0)
+#' sim$place_cell("A", 1e3, 1e3)
+#' sim$run_up_to_size("A",1e4)
+#' sim$add_mutant(name = "B",
+#'                growth_rates = 3.5,
+#'                death_rates = 0)
+#' sim$mutate_progeny(sim$choose_cell_in("A"), "B")
+#' sim$run_up_to_size("B",1e4)
+#' bbox <- sim$search_sample(c("A" = 100,"B" = 100), 50, 50)
+#' sim$sample_cells("Sampling", bbox$lower_corner, bbox$upper_corner)
 #' forest = sim$get_samples_forest()
-#' forest$get_samples_info()
 #' labels = get_events_table(forest)
 #' plot_sticks(forest, labels)
 
--- R/plot_vaf.R
+++ R/plot_vaf.R
@@ -12,20 +12,35 @@
 #' @export
 #'
 #' @examples
-#' sim <- new(Simulation)
-#' sim$add_mutant(name = "A", growth_rates = 0.08, death_rates = 0.01)
-#' sim$place_cell("A", 500, 500)
-#' sim$run_up_to_time(60)
-#' sim$sample_cells("MySample", c(500, 500), c(510, 510))
+#'sim <- new(Simulation, "homogeneous_test",seed = sample(x = 1:5000,size = 1,replace = F),
+#'            save_snapshot = F)
+#' sim$duplicate_internal_cells <- T
+#' sim$update_tissue("Liver", 2e3, 2e3)
+#' sim$add_mutant(name = "A",
+#'                growth_rates = 1,
+#'                death_rates = 0)
+#' sim$place_cell("A", 1e3, 1e3)
+#' sim$run_up_to_size("A",1e4)
+#' sim$add_mutant(name = "B",
+#'                growth_rates = 3.5,
+#'                death_rates = 0)
+#' sim$mutate_progeny(sim$choose_cell_in("A"), "B")
+#' sim$run_up_to_size("B",1e4)
+#' bbox <- sim$search_sample(c("A" = 100,"B" = 100), 50, 50)
+#' sim$sample_cells("Sampling", bbox$lower_corner, bbox$upper_corner)
 #' forest = sim$get_samples_forest()
-#' forest$get_samples_info()
+#' labels = get_events_table(forest)
 #' m_engine = build_mutation_engine(setup_code = "demo")
 #' m_engine$add_mutant(mutant_name = "A",
 #' passenger_rates = c(SNV = 5e-8),
 #' driver_SNVs = c(),
 #' driver_CNAs = c())
+#' m_engine$add_mutant(mutant_name = "B",
+#' passenger_rates = c(SNV = 5e-8),
+#' driver_SNVs = c(),
+#' driver_CNAs = c())
 #' m_engine$add_exposure(time = 0, c(SBS1 = 0.2,SBS5 = 0.8)) 
-#' phylo_forest = m_engine$place_mutations(frel, 100)
+#' phylo_forest = m_engine$place_mutations(forest, 100)
 #' labels = get_events_table(forest)
 #' seq_results = simulate_seq(
 #' phylo_forest,
@@ -34,6 +49,7 @@
 #' write_SAM = F)
 #' muts = muts_to_sticks(phylo_forest,seq_results,labels)
 #' plot_vaf(muts,vaf_cut = 0.02, color_by = "stick")
+
 
 plot_vaf = function(muts,vaf_cut = 0.02, color_by = "stick"){
 
